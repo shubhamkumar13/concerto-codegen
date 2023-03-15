44ceff0665f0c6e4fc0d4dbedb70b07c1d5fc455
Martin Halford <martin@mantradao.com>
2023-03-15 17:27:24 +1100

    Fix testharness to properly import CTO files.
    Signed-off-by: Martin Halford <martin@mantradao.com>

--- testharness.js
+++ testharness.js
@@ -1,29 +1,38 @@
-'use strict';
-
 const fs = require('fs');
-
 const { ModelManager } = require('@accordproject/concerto-core');
 const { FileWriter } = require('@accordproject/concerto-util');
-
 const Visitor = require('./lib/codegen/fromcto/rust/rustvisitor');
 
-//const modelPath = './testharness/model-helloworld.cto';
-const modelPath = './testharness/model-minimal.cto';
-// const modelPath = './testharness/contract.cto';
+try {
+  const modelFilePath = process.argv[2];
+  if (!modelFilePath) { // ensure model file path is provided
+    console.log('Usage: node testharness.js <model filepath & filename> <output filepath>');
+    process.exit(1);
+  }
+  const model = fs.readFileSync(modelFilePath, 'utf8');
+  const mm = new ModelManager();
 
-const model = fs.readFileSync(modelPath, 'utf8');
+  mm.addCTOModel(model, modelFilePath, true);  // true to disable consistency checks on imports
+  mm.updateExternalModels();
 
-const modelManager = new ModelManager();
+  let outputFilePath = process.argv[3];
+  if (!outputFilePath) {  // ensure output file path is provided
+    console.log('Usage: node testharness.js <model file> <output file>');
+    process.exit(1);
+  };
+  if (!outputFilePath.endsWith('/')) {  // ensure output file path ends with '/'
+    outputFilePath += '/';
+  };
 
-modelManager.addCTOModel(model);
-// modelManager.addModelFiles(['./testharness/contract.cto', './testharness/model-helloworld.cto', './testharness/runtime.cto']);
-modelManager.updateExternalModels();
-modelManager.validateModelFiles();
+  const parameters = {
+    fileWriter: new FileWriter(outputFilePath)
+  };
+  const visitor = new Visitor();
 
-const parameters = {
-  fileWriter: new FileWriter('./output/rust')
-};
-const visitor = new Visitor();
+  mm.accept(visitor, parameters);
+  console.log('Code generation complete. Output files are in ' + outputFilePath + '.');
 
-modelManager.accept(visitor, parameters);
-console.log('Done');
+}
+catch (err) {
+  console.log(err)
+}
